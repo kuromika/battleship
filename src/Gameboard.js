ccce69c82ced50843ffd0bb4bdeb9573bb1437e0
mika-alpha <yuukeikioku@gmail.com>
2022-10-19 18:54:08 -0500

    Add placeShip method and auxiliar methods to Gameboard

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,8 +1,85 @@
+import Ship from "./Ship";
 
-const Gameboard = () => {
+class Gameboard {
+
+    #matrix = [];
+
+    #ships = [];
+
+    constructor() {
+        this.#initialize();
+    }
+
+    #initialize() {
+        this.#createMatrix();
+    }
+    
+    #createMatrix() {
+        for (let i = 0; i < 10; i += 1){
+            this.#matrix.push([]);
+            for (let j = 0; j < 10; j += 1){
+                this.#matrix[i].push(6);
+            }
+        }
+    }
+
+    placeShip(x, y, length, orientation) {
+        if (orientation === 'horizontal') {
+            return this.#placeHorizontal(x, y, length);
+        }
+        return this.#placeVertical(x, y, length);
+    }
+
+    #isValidHorizontal(x, y, length) {
+        if (x + length < 10) {
+            for (let i = y; i < y + length; i += 1){
+                if (this.#matrix[x][i] !== 6) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    #placeHorizontal(x, y, length) {
+        if (this.#isValidHorizontal(x, y, length)) {
+            for (let i = y; i < y + length; i += 1) {
+                this.#matrix[x][i] = this.#ships.length;
+            }
+            this.#ships.push(new Ship(length));
+            return true;
+        }
+        return false;
+    }
+
+    #isValidVertical(x, y, length) {
+        if (y + length < 10) {
+            for (let i = y; i < y + length; i += 1){
+                if (this.#matrix[x][i] !== 6){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    #placeVertical(x, y, length) {
+        if (this.#isValidVertical) {
+            for (let i = x; i < x + length; i += 1){
+                this.#matrix[i][y] = this.#ships.length;
+            }
+            this.#ships.push(new Ship(length));
+            return true;
+        }
+        return false;   
+    }
 
 
 
+    getValueAt(x, y) {
+        return this.#matrix[x][y];
+    }
 }
-
 export default Gameboard;
--- test/Gameboard.test.js
+++ test/Gameboard.test.js
@@ -5,14 +5,14 @@ describe('Gameboard tests', () => {
     let testBoard;
 
     beforeEach(() => {
-        testBoard = Gameboard();
+        testBoard = new Gameboard();
     })
 
     test("Ship is placed horizontally in expected coordinates", () => {
         testBoard.placeShip(0, 0, 5, 'horizontal');
         let flag = true;
-        for (let j = 0; j < 5; j++){
-            if (testBoard.getValueAt(j,0) !== 1) {
+        for (let i = 0; i < 5; i += 1){
+            if (testBoard.getValueAt(0, i) !== 0) {
                 flag = false;
                 break;
             }
@@ -21,10 +21,10 @@ describe('Gameboard tests', () => {
     })
 
     test("Ship is placed vertically in expected coordinates", () => {
-        testBoard.placeShip(1, 0, 3, 'vertical');
+        testBoard.placeShip(0, 1, 3, 'vertical');
         let flag = true;
         for (let i = 0; i < 3; i++){
-            if (testBoard.getValueAt(1,i) !== 1) {
+            if (testBoard.getValueAt(i,1) !== 0) {
                 flag = false;
                 break;
             }
@@ -34,13 +34,13 @@ describe('Gameboard tests', () => {
 
     test("Ship can't be placed out of bounds", () => {
         testBoard.placeShip(7, 7, 5, 'horizontal');
-        expect(testBoard.getValueAt(7, 7)).toBe(0);
+        expect(testBoard.getValueAt(7, 7)).toBe(6);
     })
 
-    test("Ship can't be placed on top of other", () => {
+    test.only("Ship can't be placed on top of other", () => {
         testBoard.placeShip(0, 0, 5, 'horizontal');
         testBoard.placeShip(1, 0, 5, 'vertical');
-        expect(testBoard.getValueAt(1, 1)).toBe(0);
+        expect(testBoard.getValueAt(1, 1)).toBe(6);
     })
 
     test("Attack is registered correctly and ship is hit", () => {
@@ -51,7 +51,7 @@ describe('Gameboard tests', () => {
 
     test('Attack is registred correctly but hits nothing', () => {
         testBoard.receiveAttack(3, 3);
-        expect(testBoard.getValueAt(3, 3)).toBe(3);
+        expect(testBoard.getValueAt(3, 3)).toBe(7);
     })
 
 })
